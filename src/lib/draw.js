a1817d835adaab097198c8ddf6a2e687185003ca
Cedric Dupont <ceddup@gmail.com>
2018-05-18 15:01:09 +0200

    Textured text and QR, Drawings handled with promises

--- src/lib/draw.js
+++ src/lib/draw.js
@@ -2,8 +2,11 @@ const drawModuleRounded = require('./drawRounded');
 const drawMode = require('./drawMode');
 
 const drawBackground = (context, settings) => {
-    context.fillStyle = settings.back;
-    context.fillRect(0, 0, settings.size * settings.width, settings.size);
+	return new Promise(function(resolve) {
+        context.fillStyle = settings.back;
+        context.fillRect(0, 0, settings.size * settings.width, settings.size);
+        resolve();
+    });
 };
 
 const drawModuleDefault = (qr, context, settings, width, row, col) => {
@@ -13,36 +16,49 @@ const drawModuleDefault = (qr, context, settings, width, row, col) => {
 };
 
 const drawModules = (qr, context, settings) => {
-    if (!qr) {
-        return;
-    }
-
-    const drawModule = settings.rounded > 0 && settings.rounded <= 100 ? drawModuleRounded : drawModuleDefault;
-    const moduleCount = qr.moduleCount;
-
-    let moduleSize = settings.size / moduleCount;
-    let offset = 0;
-    if (settings.crisp) {
-        moduleSize = Math.floor(moduleSize);
-        offset = Math.floor((settings.size - moduleSize * moduleCount) / 2);
-    }
-
-    context.translate(offset, offset);
-    context.beginPath();
-    for (let row = 0; row < moduleCount; row += 1) {
-        for (let col = 0; col < moduleCount; col += 1) {
-            drawModule(qr, context, settings, moduleSize, row, col);
+	return new Promise(function(resolve) {
+        if (!qr) {
+            return;
         }
-    }
-    context.fillStyle = settings.fill;
-    context.fill();
-    context.translate(-offset, -offset);
+
+        const drawModule = settings.rounded > 0 && settings.rounded <= 100 ? drawModuleRounded : drawModuleDefault;
+        const moduleCount = qr.moduleCount;
+
+        let moduleSize = settings.size / moduleCount;
+        let offset = 0;
+        if (settings.crisp) {
+            moduleSize = Math.floor(moduleSize);
+            offset = Math.floor((settings.size - moduleSize * moduleCount) / 2);
+        }
+
+        context.translate(offset, offset);
+        context.beginPath();
+        for (let row = 0; row < moduleCount; row += 1) {
+            for (let col = 0; col < moduleCount; col += 1) {
+                drawModule(qr, context, settings, moduleSize, row, col);
+            }
+        }
+        context.fillStyle = settings.fill;
+        var img = document.createElement('img');
+        img.src='https://news.files.bbci.co.uk/include/shorthand/40076/media/sea-l_gbqwvrs-mr_4pkq6di.jpg';
+        //draw the text
+        img.onload= function(){
+            drawQR(context, img);
+            context.translate(-offset, -offset);
+            resolve();
+        }
+    });
+};
+
+function drawQR(ctx, img) {
+    ctx.fillStyle = ctx.createPattern(img, 'repeat');
+    ctx.fill();
 };
 
 const draw = (qr, context, settings) => {
-    drawBackground(context, settings);
-    drawModules(qr, context, settings);
-    drawMode(context, settings);
+    drawBackground(context, settings)
+    .then(x => drawModules(qr, context, settings))
+    .then(x => drawMode(context, settings));
 };
 
 module.exports = draw;
--- src/lib/drawMode.js
+++ src/lib/drawMode.js
@@ -1,52 +1,108 @@
-const drawLabel = (context, settings, item) => {
-    if (item.label) {
-        const size = settings.size;
-        const font = item.msize * 0.01 * size + 'px ' + item.fontname;
+const drawLabel = (item, context, settings) => {
+	return new Promise(function(resolve) {
+        if (item.label) {
+            const size = settings.size;
+            const font = item.msize * 0.01 * size + 'px ' + item.fontname;
 
-        context.strokeStyle = settings.back;
-        context.lineWidth = item.msize * 0.01 * size * 0.1;
-        context.fillStyle = item.fontcolor;
-        context.font = font;
+            context.strokeStyle = settings.back;
+            context.lineWidth = item.msize * 0.01 * size * 0.1;
+            context.fillStyle = item.fontcolor;
+            context.font = font;
 
-        const w = context.measureText(item.label).width;
-        const sh = item.msize * 0.01;
-        const sw = w / (settings.width * size);
-        const sl = (1 - sw) * (item.mposx * settings.width) * 0.01;
-        const st = (1 - sh) * item.mposy * 0.01;
-        const x = sl * size;
-        const y = st * size + 0.75 * item.msize * 0.01 * size;
+            const w = context.measureText(item.label).width;
+            const sh = item.msize * 0.01;
+            const sw = w / (settings.width * size);
+            const sl = (1 - sw) * (item.mposx * settings.width) * 0.01;
+            const st = (1 - sh) * item.mposy * 0.01;
+            const x = sl * size;
+            const y = st * size + 0.75 * item.msize * 0.01 * size;
 
-        if (item.stroke) context.strokeText(item.label, x, y);
-        context.fillText(item.label, x, y);
+            if (item.stroke) context.strokeText(item.label, x, y);
+            if (item.label === 'Pay with NANO'){
+                drawTexturedText(resolve, context, item, 'http://www.bittbox.com/wp-content/uploads/2008/04/free_hires_wood_texture_5.jpg', x, y);
+            } else {
+                context.fillText(item.label, x, y);
+                resolve();
+            }
+        } else {
+            resolve();
+        }
+    });
+};
+
+function drawTexturedText(resolve, ctx, item, imgsrc, x, y) {
+	var img = document.createElement("img");
+    img.onload= function(){
+        drawTexturedTextWhenImageLoaded(resolve, ctx, item, img, x, y);
     }
+    img.src=imgsrc;
+}
+
+function drawTexturedTextWhenImageLoaded(resolve, ctx, item, img, x, y) {
+    var can1 = getNewCanvas(ctx.canvas);
+    var ctx1 = can1.getContext("2d");
+
+    ctx1.strokeStyle = ctx.strokeStyle;
+    ctx1.lineWidth = ctx.lineWidth;
+    ctx1.fillStyle = ctx.fillStyle;
+    ctx1.font = ctx.font;
+    ctx1.fillText(item.label, x, y);
+    ctx1.globalCompositeOperation = "source-in";
+    ctx1.drawImage(img, 0, 0, img.width, img.height, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(can1, 0, 0);
+    document.body.removeChild(can1);
+    resolve();
+}
+
+// call document.body.removeChild(<returnedcanvas>); when done
+function getNewCanvas(canvas) {
+  var canv = document.createElement('canvas');
+  canv.id = 'tempcanvas';
+  canv.height = canvas.height;
+  canv.width = canvas.width;
+  canv.style.display = 'none';
+  document.body.appendChild(canv);
+  return document.getElementById('tempcanvas');
+}
+
+const drawImage = (item, context, settings) => {
+	return new Promise(function(resolve) {
+        const size = settings.size;
+        const w = item.image.naturalWidth || 1;
+        const h = item.image.naturalHeight || 1; 
+        const sh = item.msize * 0.01;
+        const sw = sh * w / (h * settings.width);
+        const sl = (1 - sw) * item.mposx * 0.01;
+        const st = (1 - sh) * item.mposy * 0.01;
+        const x = sl * size * settings.width;
+        const y = st * size;
+        const iw = sw * size * settings.width;
+        const ih = sh * size;
+
+        context.drawImage(item.image, x, y, iw, ih);
+        resolve();
+    });
 };
 
-const drawImage = (context, settings, item) => {
-    const size = settings.size;
-    const w = item.image.naturalWidth || 1;
-    const h = item.image.naturalHeight || 1; 
-    const sh = item.msize * 0.01;
-    const sw = sh * w / (h * settings.width);
-    const sl = (1 - sw) * item.mposx * 0.01;
-    const st = (1 - sh) * item.mposy * 0.01;
-    const x = sl * size * settings.width;
-    const y = st * size;
-    const iw = sw * size * settings.width;
-    const ih = sh * size;
+const drawItem = (item, context, settings) => {
+    if (item.mode === 'label') {
+        return drawLabel(item, context, settings);
+    } else if (item.mode === 'image') {
+        return drawImage(item, context, settings);
+    }
+}
 
-    context.drawImage(item.image, x, y, iw, ih);
-};
+const drawItemPromise = (item, context, settings) =>
+  drawItem(item, context, settings).then(() => {})
 
 const drawMode = (context, settings) => {
-    for (const item of settings.items) {
-        if (item) {
-            if (item.mode === 'label') {
-                drawLabel(context, settings, item);
-            } else if (item.mode === 'image') {
-                drawImage(context, settings, item);
-            }
-        }
-    }
+	return new Promise(function(resolve) {
+        settings.items.reduce(
+            (p, item) =>
+                p.then(_ => drawItemPromise(item, context, settings)),
+                Promise.resolve()
+        );
+    });
 };
 
 module.exports = drawMode;
